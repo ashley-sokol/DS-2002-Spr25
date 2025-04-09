50947b717a5fabad1a1f7f52b05204b30eeb7698
Your Name <you@example.com>
2025-04-09 00:51:46 -0400

    updated output

--- mywork/Lab_7/upload_presign.py
+++ mywork/Lab_7/upload_presign.py
@@ -5,16 +5,16 @@ from urllib.parse import urlparse
 
 s3 = boto3.client('s3', region_name='us-east-1')
 
-def fetch_and_upload_file(url, bucket_name, expiration_time):
+def fetch_and_upload_file(url, bucket_name, expires_in):
     filename = os.path.basename(urlparse(url).path)
     urllib.request.urlretrieve(url, filename)
     with open(filename, 'rb') as data:
         s3.put_object(Body=data, Bucket=bucket_name, Key=filename)
 
-    presigned_url = s3.generate_presigned_url(
-        'get_object',
-        Params={'Bucket': bucket_name, 'Key': filename},
-        ExpiresIn=expiration_time
-    )
-
+    response = s3.generate_presigned_url(
+    'get_object',
+    Params={'Bucket': bucket_name, 'Key': filename},
+    ExpiresIn=expires_in
+)
+    print(f"Presigned URL: {response}")
     return presigned_url
